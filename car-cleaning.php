faf044dffb93f88dc3a7575ee8de2bdfb26db483
sameclean <sameclean77@gmail.com>
2025-11-07 19:01:18 +0100

    Create car-cleaning.html

---
+++ car-cleaning.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Redirecting...</title>
+  <script>
+    // Redirect to new page
+    window.location.href = "/new-page.html";
+  </script>
+</head>
+<body>
+  <p>If you are not redirected automatically, <a href="/new-page.html">click here</a>.</p>
+</body>
+</html>
