c9ecd94249e65ef3928397a3e2a3d075601f86f4
sameclean <sameclean77@gmail.com>
2025-11-07 18:31:22 +0100

    Create sofa-cleaning.php

---
+++ sofa-cleaning.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="refresh" content="0; url=/sofa-cleaning-services-addis-ababa.html">
+  <link rel="canonical" href="https://sameclean.com/sofa-cleaning-services-addis-ababa.html">
+  <title>Redirecting...</title>
+</head>
+<body>
+  <p>You are being redirected to <a href="/sofa-cleaning-services-addis-ababa.html">/sofa-cleaning-services-addis-ababa.html</a></p>
+</body>
+</html>
